7ab41bb5f0d58a932afd7ebefecd33eb871db9f1
HAMS10 <hmedinasantacruz@gmail.com>
2021-04-10 16:40:25 -0500

    0.0.3

--- adjust_indexes.m
+++ adjust_indexes.m
@@ -1,4 +1,6 @@
 function [index_1, index_2] = adjust_indexes(sequence_1, sequence_2, k)
+    is_k_negative = k < 0;
+
     k = abs(k);
     index_1.start = k + 1;
     index_2.start = 1;
@@ -6,11 +8,11 @@ function [index_1, index_2] = adjust_indexes(sequence_1, sequence_2, k)
         index_1.end = length(sequence_1.Sequence);
         index_2.end = length(sequence_2.Sequence);
     elseif length(sequence_1.Sequence) - index_1.start > length(sequence_2.Sequence)
-        index_1.end = index_1.start + length(sequence_2.Sequence);
-        index_2.end = length(sequence_2.Sequence) + 1;
+        index_1.end = index_1.start + length(sequence_2.Sequence) - ~is_k_negative;
+        index_2.end = length(sequence_2.Sequence) + is_k_negative;
     else
         index_1.end = length(sequence_1.Sequence);
-        index_2.end = length(sequence_1.Sequence) - index_1.start + 1;
+        index_2.end = length(sequence_1.Sequence) - index_1.start + is_k_negative;
     end
 end
 
--- find_homologous_zones_of.m
+++ find_homologous_zones_of.m
@@ -1,5 +1,5 @@
 function [homologous_zones] = find_homologous_zones_of(sequence_1, sequence_2, c_k, diagonals_2_search)
-    window_size = 30;
+    windows_size = 30;
     
     max_number_of_windows = floor(max(length(sequence_1.Sequence), length(sequence_2.Sequence)) / 30);
         
@@ -10,6 +10,12 @@ function [homologous_zones] = find_homologous_zones_of(sequence_1, sequence_2, c
         
     for k_index = 1 : length(k_values)
         k = k_values(k_index);
-        [seq_1_index, seq_2_index] = adjust_indexes(sequence_1, sequence_2, k)
+        if k >= 0
+            [sequence_1_index, sequence_2_index] = adjust_indexes(sequence_1, sequence_2, k);
+            [find_homologous_zones_in_k] = find_homologous_zones_in(k, sequence_1, sequence_2, sequence_1_index, sequence_2_index, windows_size);
+        else
+            [sequence_2_index, sequence_1_index] = adjust_indexes(sequence_2, sequence_1, k);
+            [find_homologous_zones_in_k] = find_homologous_zones_in(k, sequence_2, sequence_1, sequence_2_index, sequence_1_index, windows_size);
+        end
     end
 end
