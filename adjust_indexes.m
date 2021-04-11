7c7116db36bdcb4f8efe651545f5b7b003f713a5
HAMS10 <hmedinasantacruz@gmail.com>
2021-04-10 21:24:10 -0500

    0.0.4

--- adjust_indexes.m
+++ adjust_indexes.m
@@ -1,18 +1,20 @@
 function [index_1, index_2] = adjust_indexes(sequence_1, sequence_2, k)
-    is_k_negative = k < 0;
-
     k = abs(k);
     index_1.start = k + 1;
     index_2.start = 1;
-    if length(sequence_1.Sequence) - index_1.start == length(sequence_2.Sequence)
-        index_1.end = length(sequence_1.Sequence);
+    if k == 0
+        max_index = min(length(sequence_1.Sequence), length(sequence_2.Sequence));
+        index_1.end = max_index;
+        index_2.end = max_index;
+    elseif length(sequence_1.Sequence) - index_1.start == length(sequence_2.Sequence)
+        index_1.end = length(sequence_1.Sequence) - 1;
         index_2.end = length(sequence_2.Sequence);
     elseif length(sequence_1.Sequence) - index_1.start > length(sequence_2.Sequence)
-        index_1.end = index_1.start + length(sequence_2.Sequence) - ~is_k_negative;
-        index_2.end = length(sequence_2.Sequence) + is_k_negative;
+        index_1.end = index_1.start + length(sequence_2.Sequence) - 1;
+        index_2.end = length(sequence_2.Sequence);
     else
         index_1.end = length(sequence_1.Sequence);
-        index_2.end = length(sequence_1.Sequence) - index_1.start + is_k_negative;
+        index_2.end = length(sequence_1.Sequence) - k;
     end
 end
 
--- homologous_zones_MAFFT_finder_DNA.m
+++ homologous_zones_MAFFT_finder_DNA.m
@@ -6,9 +6,9 @@ global PAM200
 base_index = containers.Map({'a', 't', 'g', 'c'}, 1:4);
 
 PAM200 = [2.22 -1.39 -1.46 -1.86;
-         -1.39  1.65 -1.74 -1.05;
-         -1.46 -1.74  1.03 -2.48;
-         -1.86 -1.05 -2.48  1.16];
+    -1.39  1.65 -1.74 -1.05;
+    -1.46 -1.74  1.03 -2.48;
+    -1.86 -1.05 -2.48  1.16];
 
 
 input_sequences = fastaread('balibase_mdsa_100s/RV12_BBS12039.afa', 'IgnoreGaps', true);
@@ -18,11 +18,6 @@ input_sequences = fastaread('balibase_mdsa_100s/RV12_BBS12039.afa', 'IgnoreGaps'
 %input_sequences = fastaread('balibase_mdsa_100s/RV11_BBS11002.afa', 'IgnoreGaps', true);
 %input_sequences = fastaread('balibase_mdsa_all/RV11_BB11025.afa', 'IgnoreGaps', true);
 
-%input_sequences = fastaread('msa.afa');
-
-sequence_1 = input_sequences(1);
-sequence_2 = input_sequences(2);
-
 %sequence_ = fastaread('orchidaceae_family/mRNA/vanilla_planifolia.fasta', 'IgnoreGaps', true);
 %sequence_ = fastaread('orchidaceae_family/mRNA/Phalaenopsis_hybrid_cultivar.fasta', 'IgnoreGaps', true);
 %sequence_ = fastaread('orchidaceae_family/mRNA/phalaenopsis_equestris.fasta', 'IgnoreGaps', true);
@@ -31,6 +26,12 @@ sequence_2 = input_sequences(2);
 %sequence_ = fastaread('orchidaceae_family/mRNA/dendrobium_huoshanense.fasta', 'IgnoreGaps', true);
 %sequence_ = fastaread('orchidaceae_family/mRNA/dendrobium_catenatum.fasta', 'IgnoreGaps', true);
 %sequence_ = fastaread('orchidaceae_family/mRNA/apostasia_shenzhenica.fasta', 'IgnoreGaps', true);
+
+%input_sequences = fastaread('msa.afa');
+sequence_1 = input_sequences(1);
+sequence_2 = input_sequences(2);
+
+
 
 size_sequence_1 = length(sequence_1.Sequence);
 size_sequence_2 = length(sequence_2.Sequence);
